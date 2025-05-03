0eebbc67112a3fac76018702de045ea49e0461b3
sepehrmdn77 <sepehrmaadani98@gmail.com>
2025-05-03 14:04:08 +0300

    complex features in retrieve

--- app/tasks/routes.py
+++ app/tasks/routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Path, Depends, HTTPException
+from fastapi import APIRouter, Path, Depends, HTTPException, Query
 from fastapi.responses import JSONResponse
 from tasks.schemas import * # better not to use *
 from tasks.models import TaskModel
@@ -10,9 +10,17 @@ router = APIRouter(tags=["tasks"], prefix="/todo" )
 
 
 @router.get("/tasks", response_model=List[TaskResponseSchema])
-async def retrieve_tasks_list(db:Session = Depends(get_db)):
-    result = db.query(TaskModel).all()
-    return result
+async def retrieve_tasks_list(
+    limit: int = Query(10,gt=0,le=50,description="Tasks count filter"),
+    offset: int = Query(0,gt=0,description="User for paginating based on passed items"),
+    completed: bool = Query(None,description="Filter tasks by complition status"),
+    db:Session = Depends(get_db)
+    ):
+    query = db.query(TaskModel)
+    if completed is not None:
+        query = query.filter_by(is_completed=completed)
+
+    return query.limit(limit).offset(offset).all()
 
 @router.get("/tasks/{task_id}", response_model=TaskResponseSchema)
 async def retrieve_task_detail(task_id:int = Path(...,gt=0),db:Session = Depends(get_db)):
