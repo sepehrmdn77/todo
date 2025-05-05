04ab97989325bca339e67f57623094e009bff4e9
sepehrmdn77 <sepehrmaadani98@gmail.com>
2025-05-05 11:50:16 +0300

    GutHub Actions CI test stage add

--- app/tasks/models.py
+++ app/tasks/models.py
@@ -1,19 +1,34 @@
-from sqlalchemy import Column, Integer, String, Boolean, Text, func, DateTime, ForeignKey
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    Boolean,
+    Text,
+    func,
+    DateTime,
+    ForeignKey,
+)
 from sqlalchemy.orm import relationship
 from core.database import Base
+
 # from app.users.models import UsersModel
 
 
 class TaskModel(Base):
     __tablename__ = "tasks"
     id = Column(Integer, primary_key=True, autoincrement=True)
-
-    user_id = Column(Integer,ForeignKey("users.id")) # for connecting to users table
+    # for connecting to users table
+    user_id = Column(Integer, ForeignKey("users.id"))
 
     title = Column(String(150), nullable=False)
     description = Column(Text(500), nullable=True)
     is_completed = Column(Boolean, default=False, nullable=False)
     created_date = Column(DateTime, server_default=func.now(), nullable=False)
-    updated_date = Column(DateTime, server_default=func.now(), server_onupdate=func.now(), nullable=False)
+    updated_date = Column(
+        DateTime,
+        server_default=func.now(),
+        server_onupdate=func.now(),
+        nullable=False
+    )
 
     user = relationship("UsersModel", back_populates="tasks", uselist=False)
